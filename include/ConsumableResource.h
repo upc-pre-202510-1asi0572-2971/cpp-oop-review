0b11c703e130145b30750824b894016e228d8497
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-02 08:45:06 -0500

    feat(resource): add consumable resource type.

---
+++ include/ConsumableResource.h
@@ -0,0 +1,18 @@
+#ifndef CONSUMABLERESOURCE_H
+#define CONSUMABLERESOURCE_H
+
+#include "Resource.h"
+
+class ConsumableResource final : public Resource {
+private:
+  int totalCapacity;
+  int remainingCapacity;
+public:
+  ConsumableResource(const std::string& name, int capacity);
+  bool isAvailableForUse() const override;
+  void allocate() override;
+  void release() override;
+  void use() override;
+  int getRemainingCapacity() const;
+};
+#endif //CONSUMABLERESOURCE_H
