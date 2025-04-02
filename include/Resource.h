a4735f531b6e0cb986ddab3d2c17ca5780fe16fd
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-02 08:33:56 -0500

    feat(resource): add resource abstract type.

---
+++ include/Resource.h
@@ -0,0 +1,23 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include <string>
+
+class Resource {
+public:
+    enum class Type { Consumable, Usable };
+protected:
+    std::string name;
+    bool isAvailable;
+    Type resourceType;
+public:
+  Resource(std::string name, Type type);
+  virtual ~Resource() = default;
+  std::string getName() const;
+  virtual bool isAvailableForUse() const = 0;
+  virtual void allocate() = 0;
+  virtual void relase() = 0;
+  virtual void use() = 0;
+  Type getResourceType() const;
+};
+#endif //RESOURCE_H
