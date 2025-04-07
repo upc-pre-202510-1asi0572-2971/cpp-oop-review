6c61ca3ca8ef75041f43e148d153f05a93898330
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-07 09:28:41 -0500

    feat(main): add initial main behavior.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include "Process.h"
+#include "Task.h"
+#include "UsableResource.h"
+#include "ConsumableResource.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    try {
+        const auto compilationProcess = std::unique_ptr<Process>(
+            new Process(
+                "CompileMain",
+                "Compile main.cpp to main.exe",
+                { "CentralProcessingUnit", "Memory"},
+                15));
+    } catch (const std::exception &e) {
+        std::cerr << "Critical error: " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
